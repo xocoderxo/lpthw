e7ec17b33ef035b0dbe23c35b0b4a7611795f3cb
noveyjosay <noveyjosay@gmail.com>
2015-04-01 23:00:42 +0000

    Exercise 2

---
+++ ex/ex2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# This is a comment, it helps making sense of code written in your program
+
+# we can use comments to explain what a line (or many) of code does by adding a comment above it
+
+# prints 'yay!'
+
+print "'yay!'"
+
+# another way to print 'yay!'
+
+print '\'yay!\''
+
+# i think the first one is better, will find out more as i progress with this book i guess.
